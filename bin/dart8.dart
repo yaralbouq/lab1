8e561c8f84dafde9b4c31ad68ebb9cccee06b925
yara albouq <yaramalbouq@gmail.com>
2024-07-31 10:50:47 +0300

    first commit

---
+++ bin/dart8.dart
@@ -0,0 +1,5 @@
+import 'package:dart8/dart8.dart' as dart8;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart8.calculate()}!');
+}
---
+++ test/dart8_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart8/dart8.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
